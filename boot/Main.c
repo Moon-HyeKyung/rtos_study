00adf48d7ecf34e5e797cc80935a516de3485b5b
Moon-Hyekyung <Moon-HyeKyung@github.com>
2021-03-17 13:22:22 +0900

    Add Timer Function in Main(Step 4. delay)

--- boot/Main.c
+++ boot/Main.c
@@ -2,8 +2,13 @@
 #include "HalUart.h"
 
 #include "stdio.h"
+#include "stdbool.h"
+
+#include "HalTimer.h"
+#include "stdlib.h"
 
 static void Hw_init(void);
+static void Timer_test(void); // Timer method
 
 void main(void)
 {
@@ -17,16 +22,29 @@ void main(void)
 	Hal_uart_put_char('\n');
 
 	putstr("Hello World!\n");
+	//
+	// i = 100;
+	// while(i--)
+	// {
+	// 	uint8_t ch = Hal_uart_get_char();
+	// 	Hal_uart_put_char(ch);
+	// }
+	Timer_test(); // Timer 함수 호출
 
-	i = 100;
-	while(i--)
-	{
-		uint8_t ch = Hal_uart_get_char();
-		Hal_uart_put_char(ch);
-	}
 }
 static void Hw_init(void)
 {
     Hal_interrupt_init();
     Hal_uart_init();
+		Hal_timer_init();
+}
+
+// Timer 정적함수 정의
+static void Timer_test(void)
+{
+    while(true)
+    {
+        putstr("tic\n");
+        delay(1000);
+    }
 }
