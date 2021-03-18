5ae82035c730f826f11dfc1db19c39b18200826c
Moon-Hyekyung <Moon-HyeKyung@github.com>
2021-03-18 13:47:44 +0900

    Add Task_Ctrl_Block & TaskContext

--- boot/Main.c
+++ boot/Main.c
@@ -7,8 +7,15 @@
 #include "HalTimer.h"
 #include "stdlib.h"
 
+#include "task.h"
+
 static void Hw_init(void);
+
 static void Timer_test(void); // Timer method
+
+void User_task0(void);
+void User_task1(void);
+void User_task2(void);
 
 void main(void)
 {
@@ -47,4 +54,57 @@ static void Timer_test(void)
         putstr("tic\n");
         delay(1000);
     }
+}
+
+static void Kernel_init(void)
+{
+    uint32_t taskId;
+
+    Kernel_task_init();
+
+    taskId = Kernel_task_create(User_task0);
+    if (NOT_ENOUGH_TASK_NUM == taskId)
+    {
+        putstr("Task0 creation fail\n");
+    }
+
+    taskId = Kernel_task_create(User_task1);
+    if (NOT_ENOUGH_TASK_NUM == taskId)
+    {
+        putstr("Task1 creation fail\n");
+    }
+
+    taskId = Kernel_task_create(User_task2);
+    if (NOT_ENOUGH_TASK_NUM == taskId)
+    {
+        putstr("Task2 creation fail\n");
+    }
+}
+
+//////////////// TASKS ///////////////////////////
+void User_task0(void)
+{
+    uint32_t local = 0;
+
+    putstr("User Task #0\n");
+
+    while(true);
+}
+
+void User_task1(void)
+{
+    uint32_t local = 0;
+
+    putstr("User Task #1\n");
+
+    while(true);
+}
+
+void User_task2(void)
+{
+    uint32_t local = 0;
+
+    putstr("User Task #2\n");
+
+    while(true);
 }
